9bdb2602f590220855321593fc03e45962a600b3
scott <bonniefoo@gmail.com>
2013-02-15 23:17:15 -0500

    simple tri draw

--- glharness/main.cpp
+++ glharness/main.cpp
@@ -42,10 +42,19 @@ private:
 
 	void render()
 	{
+		glBegin(GL_TRIANGLES);
+			glColor3f(1, 0, 0);
+			glVertex3f( 0.0f, 1.0f, 0.0f);
+			glColor3f(0, 1, 0);
+			glVertex3f(-1.0f,-1.0f, 0.0f);
+			glColor3f(0, 0, 1);
+			glVertex3f( 1.0f,-1.0f, 0.0f);
+		glEnd();
 	}
 
-	void size_changed(int /* width */, int /* height */)
+	void size_changed(int width, int height)
 	{
+		glViewport(0, 0, width, height);
 	}
 };
 
